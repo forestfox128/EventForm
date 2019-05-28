c30efe9ea2f945d9fd2dae7cf0aff60f1d1f80cc
forestfox128 <basiab217@gmail.com>
2019-05-28 19:36:06 +0200

    test gh_pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
+import { HashRouter as Router} from 'react-router-dom'
 import CreateEventScreen from './Screens/CreateEventScreen';
 import SuccessScreen from './Screens/SuccessScreen';
 import './App.css';
 
 function App() {
   return (
-      <Router>
-          <Switch>
-            <Route path="/login" component={CreateEventScreen}/>
-            <Route path="/success" component={SuccessScreen}/>
-        </Switch> 
-      </Router> 
+    <div>NAnananan</div>
+      // <Router>
+      //     <Switch>
+      //       <Route path="/" component={CreateEventScreen}/>
+      //       <Route path="/success" component={SuccessScreen}/>
+      //   </Switch> 
+      // </Router> 
   );
 }
 
